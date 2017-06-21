cba31739eb722e8d260e879cad471c499a4e0bac
神楽坂玲奈 <zh99998@gmail.com>
2017-06-21 11:40:03 +0800

    Update auth.py

--- auth.py
+++ auth.py
@@ -37,12 +37,36 @@ if __name__ == '__main__':
             print 'Host Name:', host_name
 
     """
-    功能 记录类型列表 
+    功能 获取解析记录列表
     HTTP 请求方式 GET
-    URL https://www.cloudxns.net/api2/type
-    :return: String
+    URL https://www.cloudxns.net/api2/record/:domain_id?host_id=0&offset=:offset&row_num=:row_num
+    URL 参数说明
+        domain_id:域名 id
+        host_id:主机记录 id(传 0 查全部)
+        offset:记录开始的偏移，第一条记录为 0，依次类推,默认取 0
+        row_num:要获取的记录的数量， 比如获取 30 条， 则为 30,最大可取 2000
+        条,默认取 30 条.
+    :return:
+        code int 请求状态，详见附件 code 对照表
+        message String 操作信息，提示操作成功或错误信息
+        total int 总记录条数
+        offset int 记录开始的偏移
+        row_num int 要获取的记录的数量
+        data array 记录列表
+            record_id: 解析记录 id
+            host_id:主机记录 id
+            host：主机记录名
+            line_id：线路 ID
+            line_zh：中文名称
+            line_en：英文名称
+            mx：优先级
+            Value：记录值
+            Type：记录类型
+            Status：记录状态(ok 已生效 userstop 暂停)
+            create_time：创建时间
+            update_time：更新时间
     """
-    result = json.loads(api.record_list(domain_id))
+    result = json.loads(api.record_list(domain_id,0,0,2000))
 
     record_id = None
     for item in result['data']:
